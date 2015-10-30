7cfed21b0766a218f12b9609bbb7694c0b7f1caf
Shamil BadAllOff <BadAllOffsh@gmail.com>
2015-10-30 07:43:59 +0400

    Refactoring: Add Attachable concern to questions and answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,7 @@
 class Question < ActiveRecord::Base
+  include Attachable
   belongs_to :user
   has_many :answers, dependent: :destroy
-  has_many :attachments, as: :attachable, dependent: :destroy
   validates :title, :body, presence: true
 
-  accepts_nested_attributes_for :attachments, reject_if: lambda {|a| a[:file].blank? }, allow_destroy: true #Дабы сохранять данные в дочерний обект через родительский
 end
